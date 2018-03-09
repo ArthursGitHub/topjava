c162257bf0a16117acbcbae45734afe879d322ee
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-03-09 17:15:48 +0300

    [HW02] added UserUtils for user generation

---
+++ src/main/java/ru/javawebinar/topjava/util/UserUtils.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by arthur on 3/9/18.
+ */
+public class UserUtils {
+  public static final List<User> USERS = Arrays.asList(
+          new User("Alex", "mail1@mail.ru", "pswd1"),
+          new User("Bob",  "mail2@mail.ru", "pswd2"),
+          new User("Tom",  "mail3@mail.ru", "pswd3")
+  );
+}
