efe98fa099edc30fc6c085be11be12929f236083
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-03-10 04:03:46 +0300

    [HW02] MealRepository interface was adapted for multiuser application

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -5,8 +5,8 @@ import ru.javawebinar.topjava.model.Meal;
 import java.util.Collection;
 
 public interface MealRepository {
-    Meal save(Meal meal);
-    void delete(int id);
-    Meal get(int id);
-    Collection<Meal> getAll();
+    Meal save(int userId, Meal meal);
+    void delete(int userId, int id);
+    Meal get(int userId, int id);
+    Collection<Meal> getAll(int userId);
 }
