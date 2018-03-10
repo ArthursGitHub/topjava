468be7524bedff98ede997306c6c8b7c6414d476
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-03-10 04:08:09 +0300

    [HW02] InMemoryMealRepositoryImpl adapted for multiuser application

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -10,37 +10,54 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class InMemoryMealRepositoryImpl implements MealRepository {
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private Map<Integer, Map<Integer, Meal>> repo = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.MEALS.forEach(this::save);
+        final Integer[] mealId = {0};
+
+        MealsUtil.MEALS.forEach(meal -> {
+                    int userId = mealId[0] / 3;
+                    save(userId, meal);
+                    mealId[0]++;
+                }
+        );
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(int userId, Meal meal) {
+        Map<Integer, Meal> userRepo = repo.get(userId);
+        if (userRepo == null) {
+            userRepo = new ConcurrentHashMap<>();
+            repo.put(userId, userRepo);
+        }
+
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            userRepo.computeIfAbsent(meal.getId(), i -> meal);
             return meal;
         }
         // treat case: update, but absent in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        userRepo.computeIfPresent(meal.getId(), (i, meal1) -> meal);
+        return meal;
     }
 
     @Override
-    public void delete(int id) {
-        repository.remove(id);
+    public void delete(int userId, int id) {
+        Map<Integer, Meal> userRepo = repo.get(userId);
+        userRepo.remove(id);
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int userId, int id) {
+        Map<Integer, Meal> userRepo = repo.get(userId);
+        return userRepo.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(int userId) {
+        Map<Integer, Meal> userRepo = repo.get(userId);
+        return userRepo.values();
     }
 }
 
