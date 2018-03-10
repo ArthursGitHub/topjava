a09bb0e7c0638da8a718e070572ca8cb9cdd01fb
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-03-11 00:53:56 +0300

    [HW02] MealRestController marked as @Controller

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
@@ -9,6 +10,7 @@ import java.util.Collection;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
+@Controller
 public class MealRestController {
     @Autowired
     private MealService service;
