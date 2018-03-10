49a75d2e3f1314c72dbfdf20137f5714aa233e5b
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-03-10 04:01:54 +0300

    [HW02] Servlet was adapted for working with userId Meal list is sorted by time

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.repository.mock.InMemoryMealRepositoryImpl;
+import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletConfig;
@@ -15,10 +16,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Objects;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
+    private final int USER_ID = 0;
 
     private MealRepository repository;
 
@@ -39,7 +42,7 @@ public class MealServlet extends HttpServlet {
                 Integer.parseInt(request.getParameter("calories")));
 
         log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
-        repository.save(meal);
+        repository.save(USER_ID, meal);
         response.sendRedirect("meals");
     }
 
@@ -51,22 +54,29 @@ public class MealServlet extends HttpServlet {
             case "delete":
                 int id = getId(request);
                 log.info("Delete {}", id);
-                repository.delete(id);
+                repository.delete(USER_ID, id);
                 response.sendRedirect("meals");
                 break;
             case "create":
             case "update":
                 final Meal meal = "create".equals(action) ?
                         new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                        repository.get(getId(request));
+                        repository.get(USER_ID, getId(request));
                 request.setAttribute("meal", meal);
                 request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
                 break;
             case "all":
             default:
                 log.info("getAll");
-                request.setAttribute("meals",
-                        MealsUtil.getWithExceeded(repository.getAll(), MealsUtil.DEFAULT_CALORIES_PER_DAY));
+                List<MealWithExceed> withExceeded = MealsUtil.getWithExceeded(repository.getAll(USER_ID), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+                withExceeded.sort((o1, o2) -> {
+                            LocalDateTime dateTime1 = o1.getDateTime();
+                            LocalDateTime dateTime2 = o2.getDateTime();
+                            int i = -dateTime1.compareTo(dateTime2);
+                            return i;
+                        }
+                );
+                request.setAttribute("meals", withExceeded);
                 request.getRequestDispatcher("/meals.jsp").forward(request, response);
                 break;
         }
