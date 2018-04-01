714e8fa210948e4da10a4c308fc3838d7a1b05d9
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-04-01 04:21:34 +0300

    [HW5] Added tests for meal with user

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,14 +1,25 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealServiceTest;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 
 /**
  * Created by arthur on 3/28/18.
  */
-
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 public class DataJpaMealServiceTest extends MealServiceTest {
 
+  @Test
+  public void testGetWithUser() throws Exception {
+    Meal adminMeal = service.getWithUser(ADMIN_MEAL_ID, ADMIN_ID);
+    assertMatch(adminMeal, ADMIN_MEAL1);
+    UserTestData.assertMatch(adminMeal.getUser(), UserTestData.ADMIN);
+  }
 }
