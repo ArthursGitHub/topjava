5cda3c4caff2aabcb6ec76a4e1f6c438b182c403
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2018-04-23 20:52:59 +0300

    [HW07] Added MealRestControllerTest

--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -1,7 +1,27 @@
 package ru.javawebinar.topjava.web.meal;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
 
-public class MealRestControllerTest {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.MEAL1;
+import static ru.javawebinar.topjava.MealTestData.MEAL1_ID;
+import static ru.javawebinar.topjava.MealTestData.contentJson;
+
+public class MealRestControllerTest extends AbstractControllerTest {
+  private static final String REST_URL = MealRestController.REST_URL + '/';
+
+  @Test
+  public void testGet() throws Exception {
+    mockMvc.perform(get(REST_URL + MEAL1_ID))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(contentJson(MEAL1));
+  }
 
 }
