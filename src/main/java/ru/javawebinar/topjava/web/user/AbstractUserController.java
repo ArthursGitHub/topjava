39981dad660f92b5fbec09097349e4fd7e71c703
arthur <grelka.2018@yandex.ru>
2019-07-08 02:10:58 +0300

    1

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -2,9 +2,7 @@ package ru.javawebinar.topjava.web.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.service.UserService;
 
 import java.util.List;
 
@@ -14,38 +12,33 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 public abstract class AbstractUserController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
-    @Autowired
-    private UserService service;
-
     public List<User> getAll() {
         log.info("getAll");
-        return service.getAll();
+        return null;
     }
 
     public User get(int id) {
         log.info("get {}", id);
-        return service.get(id);
+        return null;
     }
 
     public User create(User user) {
         log.info("create {}", user);
         checkNew(user);
-        return service.create(user);
+        return null;
     }
 
     public void delete(int id) {
         log.info("delete {}", id);
-        service.delete(id);
     }
 
     public void update(User user, int id) {
         log.info("update {} with id={}", user, id);
         assureIdConsistent(user, id);
-        service.update(user);
     }
 
     public User getByMail(String email) {
         log.info("getByEmail {}", email);
-        return service.getByEmail(email);
+        return null;
     }
 }
